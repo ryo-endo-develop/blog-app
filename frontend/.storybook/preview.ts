5f431dfd942dd7a58321d994ee4c578576e1c587
ryo-endo-develop <r3endo.dev@gmail.com>
2024-10-24 21:34:51 +0900

    不要なクラスの削除

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,5 +1,4 @@
 import type { Preview } from '@storybook/react';
-import '../src/components/stylesheets/reset.css';
 
 const preview: Preview = {
   parameters: {
--- frontend/src/components/stylesheets/reset.css.ts
+++
@@ -1,4 +0,0 @@
-import { globalStyle } from '@vanilla-extract/css';
-import * as vars from './variables';
-
-const parentElements = ['canvas'];
